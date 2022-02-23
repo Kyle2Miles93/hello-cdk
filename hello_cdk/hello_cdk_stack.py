78045141a7de2c3845c8e3ccb7622946fa049f15
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-02-23 14:55:58 -0500

    modified s3 bucketparameters.

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,9 +1,11 @@
 from aws_cdk import (
-    Duration,
+    #Duration,
     Stack,
-    aws_sqs as sqs,
-    aws_s3 as s3
+    #aws_sqs as sqs,
 )
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+
 from constructs import Construct
 
 class HelloCdkStack(Stack):
@@ -11,9 +13,13 @@ class HelloCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        bucket = s3.Bucket(self, "MyFirstBucket", 
+            versioned=True,
+            removal_policy=cdk.REMOVALPOLICY.DESTROY,
+            auto_delete_objects=True)
+            
         # The code that defines your stack goes here
 
-        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
